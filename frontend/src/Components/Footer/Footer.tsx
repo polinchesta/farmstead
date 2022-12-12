57a9d1eef0c97faa669ceb36d88ba077337cac07
polinchesta <polinalashkevich72@gmail.com>
2022-12-12 18:51:19 +0300

    commit homePage and Grodno

---
+++ frontend/src/Components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import styles from "./Footer.module.sass"
+
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      <h2></h2>
+    </div>
+  )
+}
--- frontend/src/Components/HomePage/Home.tsx
+++ frontend/src/Components/HomePage/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Footer } from '../Footer/Footer';
+import falvarek from '../../assets/falvarek.jpeg';
 import styles from './Home.module.sass';
+import date from '../../data/cardData.json';
+import { Card } from '../Cards/Card';
 
 export function Home() {
     return (
@@ -12,6 +16,17 @@ export function Home() {
                     </Link>
                 </nav>
             </div>
+            {date.map((card, index) => (
+                <Card key={index} date={card.date} img={card.img} minutsForRead={card.minutsForRead} text={card.text} title={card.title} read={card.read}/>
+            ))}
+            <iframe
+                className={styles.video}
+                src="https://www.youtube-nocookie.com/embed/L7b5nAL5mFY"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen></iframe>
+            <Footer />
         </div>
     );
 }
